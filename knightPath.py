149fd30b29e4948cc436f8c6722b541f7ab2fc7b
andrew-conte434 <conteandrew5@gmail.com>
2022-02-07 21:56:55 -0500

    implemeted breadth first search algorithm

--- knightPath.py
+++ knightPath.py
@@ -13,7 +13,6 @@ knight to move between two squares on a chessboard.
 
 grid = []
 count = 63
-square = 63
 
 for r in range(8):
     row = []
@@ -23,14 +22,23 @@ for r in range(8):
     grid.append(row)
     
 
-def getCoordinates(sqaure, grid):
-    for i, subList in enumerate(grid):
-        if sqaure in subList:
-            return [i, subList.index(square)]
+class Node:
+    def __init__(self, square, dist=0):
+        self.square = square
+        self.dist = dist
+        
+    def __hash__(self):
+        return hash(self.square, self.dist)
     
-def getLegalMoves(grid, square, x, y):
+    def __eq__(self, other):
+        return (self.square, self.dist) == (other.square, other.dist)
+
+
+
+
+    
+def getLegalMoves(grid, x, y):
     moves = []
-    
     #defines coordinates of all possible knight moves in clockwise order
     movesX = [1, 2, 2, 1, -1, -2, -2, -1]
     movesY = [2, 1, -1, -2, -2, -1, 1, 2]
@@ -39,28 +47,49 @@ def getLegalMoves(grid, square, x, y):
         if 0 <= x + movesX[i - 1] <= 7 and 0 <= y + movesY[i - 1] <= 7: 
             moves.append(grid[x + movesX[i-1]][y + movesY[i-1]])
     
-    print(moves)
-    
     return moves
 
-def bfs(start, end):
+
+def getCoordinates(square, grid):
+    for i, subList in enumerate(grid):
+        if square in subList:
+            return [i, subList.index(square)]
+
+        
+        
+def bfs(start, end, grid):
     
-    if start == end:
-        return 0
+    queue = []
+    visited = []
     
-    searchQueue = []
-    auxQueue = []
-    count = 1
-    searchQueue.append(start)
+    root = Node(start)
+    dest = Node(end)
     
-    while true:
-        searchQueue.extend(getLegalMoves(grid, start, x, y))
+    queue.append(root)
+    
+    while queue:
+        
+        node = queue.pop(0)
+        
+        if node.square == dest.square:
+            return node.dist
+        
+        if node not in visited:
+            visited.append(node)
+            
+            coordinates = getCoordinates(node.square, grid)
+            moves = getLegalMoves(grid, coordinates[0], coordinates[1])
+            
+            for i in moves:
+                queue.append(Node(i, node.dist + 1))
+    
     
     
     
 for row in grid:
     print(row)
     
-startCoordinates = getCoordinates(square, grid)
-getLegalMoves(grid, square, startCoordinates[0], startCoordinates[1])
+start = 56
+end = 6
 
+print("Distance: %d" %bfs(start, end, grid))
