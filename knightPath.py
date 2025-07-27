76670138dcfd57d7491a12805c10b45213d225fc
Andrew Conte <conteandrew5@gmail.com>
2025-07-27 14:20:36 -0400

    Add files via upload

--- knightPath.py
+++ knightPath.py
@@ -1,62 +1,75 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 """
 
 This program will use a breadth-first search to find the shortest path for a 
 knight to move between two squares on a chessboard. 
 """
 
-grid = []
-count = 63
-
-for r in range(8):
-    row = []
-    for c in range(8):
-        row.insert(0, count)
-        count -= 1
-    grid.append(row)
-    
-
 class Node:
-    def __init__(self, square, dist=0):
+    def __init__(self, square, parent = None, dist=0):
         self.square = square
+        self.parent = parent
         self.dist = dist
+
         
     def __hash__(self):
-        return hash(self.square, self.dist)
+        return hash(self.square, self.parent, self.dist)
     
     def __eq__(self, other):
-        return (self.square, self.dist) == (other.square, other.dist)
+        if not isinstance(other, Node):
+            return False
+        else:
+            return (self.square, self.parent, self.dist) == (other.square, other.parent, other.dist)
 
 
+def main():
 
+    grid = generateGrid()
 
+    for row in grid:
+        print(row)
     
-def getLegalMoves(grid, x, y):
-    moves = []
-    #defines coordinates of all possible knight moves in clockwise order
-    movesX = [1, 2, 2, 1, -1, -2, -2, -1]
-    movesY = [2, 1, -1, -2, -2, -1, 1, 2]
+    start = input("Enter starting square: ")
+    end = input("Enter ending square: ")
+
+    if checkInput(start, end) == False:
+        print("Invalid input!")
+    else:
+        result = bfs(int(start), int(end), grid)
+        printResult(result)
     
-    for i in range(8):
-        if 0 <= x + movesX[i - 1] <= 7 and 0 <= y + movesY[i - 1] <= 7: 
-            moves.append(grid[x + movesX[i-1]][y + movesY[i-1]])
+
+
+def generateGrid():
     
-    return moves
+    grid = []
+    count = 63
 
+    for r in range(8):
 
-def getCoordinates(square, grid):
-    for i, subList in enumerate(grid):
-        if square in subList:
-            return [i, subList.index(square)]
+        row = []
 
-        
-        
+        for c in range(8):
+            row.insert(0, count)
+            count -= 1
+            
+        grid.insert(0, row)
+    
+    return grid
+
+      
+def checkInput(start, end) -> bool:
+    if not start.isnumeric() or not end.isnumeric():
+        return False
+    
+    start = int(start)
+    end = int(end)
+
+    if not start in range(0, 64) or not end in range(0, 64):
+        return False
+    
+    return True
+
+     
 def bfs(start, end, grid):
     
     queue = []
@@ -72,35 +85,50 @@ def bfs(start, end, grid):
         node = queue.pop(0)
         
         if node.square == dest.square:
-            return node.dist
+            return node
         
         if node not in visited:
             visited.append(node)
             
-            coordinates = getCoordinates(node.square, grid)
+            coordinates = [node.square // 8, node.square % 8]
             moves = getLegalMoves(grid, coordinates[0], coordinates[1])
             
             for i in moves:
-                queue.append(Node(i, node.dist + 1))
+                queue.append(Node(i, node, node.dist + 1))
+
+
+# Returns a list of all legal moves from a given position on the grid 
+def getLegalMoves(grid, x, y):
+    moves = []
+    #defines coordinates of all possible knight moves in clockwise order
+    movesX = [1, 2, 2, 1, -1, -2, -2, -1]
+    movesY = [2, 1, -1, -2, -2, -1, 1, 2]
     
-for row in grid:
-    print(row)
+    for i in range(8):
+        if 0 <= x + movesX[i - 1] <= 7 and 0 <= y + movesY[i - 1] <= 7: 
+            moves.append(grid[x + movesX[i-1]][y + movesY[i-1]])
     
-start = input("Enter starting square: ")
-end = input("Enter ending square: ")
+    return moves
 
-def checkInput(start, end, grid):
-    if not start.isnumeric() or not end.isnumeric():
-        print("Invalid input!")
-        return
-    start = int(start)
-    end = int(end)
-    if not start in range(0, 64) or not end in range(0, 64):
-        print("Invalid input!")
-        return
-    print("Distance: ",bfs(start, end, grid))
+def printResult(finalNode):
+    nodesVisited = []
+
+    node = finalNode
+    
+    # Populate path list, using 'while True' because python doesn't have do..while loops  
+    while True:
+        nodesVisited.insert(0, node.square)
+        if node.parent == None:
+            break
+        else:
+            node = node.parent
+
+    path = nodesVisited[0] if len(nodesVisited) == 1 else " -> ".join(map(str, nodesVisited))
+    
+    print("DISTANCE: ", finalNode.dist)
+    print("PATH: ", path)
 
 
+if __name__ == "__main__":
+    main()
 
-
-checkInput(start, end, grid)
